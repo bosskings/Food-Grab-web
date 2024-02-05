e8d4976910c99dd243ee821966121798291dd893
macourage18 <macourage17@gmail.com>
2024-02-05 18:35:51 +0100

    latest

--- src/pages/Navbar.js
+++ src/pages/Navbar.js
@@ -1,8 +1,18 @@
+import LOGO from "../components/media/Landscape Logo II_1- Transparent.png"
+
 export const Navbar = ()=>{
   return(
     <div>
       <nav>
-        
+      <img className={"logo1"} src={LOGO} alt=""/>
+      <div>
+        <button className={"merchantsigup"}>
+          
+        </button>
+        <button className={"merchantlogin"}>
+
+        </button>
+      </div>
       </nav>
     </div>
   )
