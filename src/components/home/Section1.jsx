956ff716095f3b34b07baeda7321fff8fd093329
macourage18 <macourage17@gmail.com>
2024-02-07 01:55:33 +0100

    latest

--- src/components/home/Section1.jsx
+++ src/components/home/Section1.jsx
@@ -1,24 +1,39 @@
 import "./home.css"
 import BIKE from "../media/Group 1.png"
+import PLAY from "../media/1664285914google-play-logo-png.png"
+import { Link } from "react-router-dom"
+import { Apple } from "react-bootstrap-icons"
 
 export const Section1 =() =>{
   
   return(
     <div>
       <section className={"sec1"}>
-        <div>
+        <div className="cont1">
         <h1 className={"txt1"}>
         Food Grab   
         </h1>
-        <h1 className={"txt1"}>
-        <span className="polygon">Delivers</span> Your
+        <h1 className={"txt1 delivers"}>
+        <div className="polygon">Delivers</div> <div>Your</div>
         </h1>
         <h1 className={"txt1"}>
           Favorites
         </h1>
         <h1 className={"txt1"}>
-          Within Minutes!
+          Within <span className={"min"}>Minutes!</span>
         </h1>
+        <div className={"buttoncontainer"}>
+          <Link>
+            <button className="ios">
+              <Apple /> Apple Store
+            </button>
+          </Link>
+          <Link>
+            <button className="andriod">
+              <img src={PLAY} alt="" /> play Store
+            </button>
+          </Link>
+        </div>
         </div>
         <div className="imagecont">
           <img className={"img1"} src={BIKE} alt=""/>
---
+++ src/components/home/section2.jsx
@@ -0,0 +1,8 @@
+export const Section2 =()=>{
+
+  return(
+    <div>
+
+    </div>
+  )
+}
