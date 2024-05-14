7081cd7677793b86a756843e3ece2aa174fdab73
Bright Philip <110538612+brightphi210@users.noreply.github.com>
2024-05-14 13:19:54 -0700

    Latest Push

--- src/components/dashboard/Shop.jsx
+++ src/components/dashboard/Shop.jsx
@@ -68,6 +68,8 @@ export const Shop = () => {
       </div>
     )
   }
+
+  
 const Shop2 =({data})=>{
   const [edit, setEdit] = useState(false)
 
--- src/components/modal/CreateShopModal.jsx
+++ src/components/modal/CreateShopModal.jsx
@@ -5,6 +5,7 @@
   import { MdVerifiedUser } from "react-icons/md";
   import { GoClock } from "react-icons/go";
   import { FaRegCalendarAlt } from "react-icons/fa";
+  import { AiOutlineCloudUpload } from "react-icons/ai";
 
 
   export const CreateShopModal = () => {
@@ -12,6 +13,7 @@
     const [imageUrl, setImageUrl] = useState(localStorage.getItem('coverImg') || '')
     const [open, setOpen] = useState(false)
     const [edit, setEdit]= useState('not-active')
+
 
     const [shop, setShop]= useState('')
   useEffect(()=>{
@@ -83,137 +85,192 @@
       setOpen(!open)
     }
 
+
+
+    const [image, setImage] = useState(null)
+    const [cover_image, setCover_image] = useState(null)
+    const [fileName, setFileName] = useState("No Selected File Name")
+
     return (
-      <div className={open ?"not-active":"createshdiv"}>
-      <div className={open ? "not-active" : "createShop"}>
-      <div>
-          <p className='txt2'>Shop Profile</p>
-        <p className='ppsubtxt'>change and edit shop profile detials</p>
-        </div>
-      <form
-      onSubmit={''}
-      method='POST'
-      className={"createshform"}>
-        <section className={"creatshopSec1"}>
-        <div className={'screatehoppfsec1'}>
-        <div className={'cvholder'} onMouseEnter={handleEdit} onMouseLeave={handleMouseLeave}>
-          <div className={edit} onClick={handleImageChange}>
-                <Camera /> <p>click to add cover image</p>
-                <input 
-                type='file'
+    <div className={open ?"not-active":"createshdiv"}>
+
+
+        <div className={open ? "not-active" : "createShop"}>
+          <div>
+            <p className='txt2'>Shop Profile</p>
+            <p className='ppsubtxt'>change and edit shop profile detials</p>
+          </div>
+
+
+          <form className="createshform">
+            <section className="creatshopSecOne"  
+              onClick={()=> document.querySelector(".input-field").click()}>
+                <div>
+                <input
+                  type="file"
+                  accept='image/*'
+                  className='input-field'
+                  hidden
+                  onChange={({ target: { files } }) => {
+                    const selectedFile = files[0];
+                    
+                    if (selectedFile) {
+                      // Check if the selected file has the correct dimensions
+                      const image = new Image();
+                      image.src = URL.createObjectURL(selectedFile);
+                      image.onload = () => {
+                        if (image.width === 1350 && image.height === 320) {
+                          setFileName(selectedFile.name);
+                          setImage(URL.createObjectURL(selectedFile));
+                          setCover_image(selectedFile);
+                        } else {
+                          // Notify the user that the image dimensions are incorrect
+                          alert('Please upload an image with dimensions 1350 by 320 pixels.');
+                        }
+                      };
+                    }
+                  }}
                 />
-              </div>
-              <div className={"coverImage update"}>
-              <img src={coverImage} alt=''/>
-              </div>
-        </div>
-        </div>
-      </section>
-      <section className={"createShopSec2"}>
-        <div className={"profileImage"}>
-        {<img src={imageUrl} alt='' className={''}/>}
-        <div className="checkboxContainer2">
-        <MdVerifiedUser className='customcheckbox2' color={shop && shop.verified ? '#0077ff' : 'grey'}/>
-        </div>
-   
-        </div>
-        <div className={"profileCredentials"}>
-          <p className={"txt3"}>Business Logo</p>
-          <p className={"subtxt2"}>This Picture will be displayed on your profile</p>
-          <labbel className='change'>
-          <input 
-          type='file' 
-          onChange={handleImageChange}
-          />
-          <AiOutlinePicture className={"ChIcon"}/>
-          Change Photo
-          </labbel>
-        </div>
-      </section>
-      <section className={"createShopSec2"}>
-        <div className={"submitShopProfile"}>
-        <p className={'txt3'}>Shop Information</p>
-        <p className='ppsubtxt cc'>update your personal detials here</p>
-        <button type='submit' className={"updPfButton"}>
-          Save Changes
-        </button>
-        </div>
-        <div className={"createShopFormProper"}>
-        <label className={"subtxt"}>Business name</label>
-        <input
-        className={"createinput"}
-          type='text'
-          name='business'
-          placeholder='business name'
-          required
-        />
-        <label className={"txt3"}>Location</label>
-        <input 
-        className={"createinput"}
-          type='text'
-          name='location'
-          placeholder='location'
-          required
-        />
-        <label className={"txt3"}>Category</label>
-        <select className={"cate"}>
-          <option>Resturant</option>
-          <option>Chef</option>
-        </select>
-         <label className={"txt3"}>Work Hours</label>
-        <div className={"work"}>
-          <div className={"workHours"}>
-          <p className={'inittxt'}>From:</p>
-          <input 
-          className='workinput'
-          type='text'
-          name='location'
-          required
-        />
-        <GoClock  className={"clock"}/>
-          </div>
-          <div className={"workHours"}>
-          <p className={'inittxt'}>To:</p>
-          <input 
-          className='workinput'
-          type='text'
-          name='location'
-          required
-        />
-        <GoClock className={"clock"}/>
-          </div>
-        </div>
-        <label className={"txt3"}>Work Days</label>
-        <div className={"work"}>
-          <div className={"workHours"}>
-          <p className={'inittxt'}>From:</p>
-          <input 
-          className='workinput'
-          type='text'
-          name='location'
-          required
-        />
-        <FaRegCalendarAlt  className={"clock"}/>
-          </div>
-          <div className={"workHours"}>
-          <p className={'inittxt'}>To:</p>
-          <input 
-          className='workinput'
-          type='text'
-          name='location'
-          required
-        />
-        <FaRegCalendarAlt className={"clock"}/>
-          </div>
-        </div>
 
-        </div>
-      </section>
 
-      </form>
-        
-      </div>
+                {image ? 
+                    <img src={image} alt=''/> : 
+                    
+                    <div className='uploadIconTextDiv'>
+                        <p className='textIcon'><AiOutlineCloudUpload /></p>
+                        <p>Upload Cover Image </p>
+                    </div>
+                }
+              </div>
+            </section>
+
+
+              <div>
+                  <section className="createShopSec2">
+                  <div className={"profileImage"}>
+                    {<img src={imageUrl} alt='' className={''}/>}
+
+                    <div className="checkboxContainer2">
+                      <MdVerifiedUser className='customcheckbox2' color={shop && shop.verified ? '#0077ff' : 'grey'}/>
+                    </div>
+            
+                  </div>
+
+                  <div className="profileCredentials">
+
+                    <p className={"txt3"}>Business Logo</p>
+                    <p className={"subtxt2"}>This Picture will be displayed on your profile</p>
+
+                    <labbel className='change'>
+                      <input type='file' onChange={handleImageChange}/>
+                      <AiOutlinePicture className={"ChIcon"}/>
+                      Change Photo
+                    </labbel>
+
+                  </div>
+
+                </section>
+
+
+                <section className={"createShopSec2"}>
+                  <div className={"submitShopProfile"}>
+                    <p className={'txt3'}>Shop Information</p>
+                    <p className='ppsubtxt cc'>update your personal detials here</p>
+                    <button type='submit' className={"updPfButton"}>Save Changes</button>
+                  </div>
+
+
+                  <div className={"createShopFormProper"}>
+
+                    <label className={"subtxt"}>Business name</label>
+                    <input
+                      className={"createinput"}
+                        type='text'
+                        name='business'
+                        placeholder='business name'
+                        required
+                    />
+
+
+                    <label className={"txt3"}>Location</label>
+                    <input 
+                      className={"createinput"}
+                        type='text'
+                        name='location'
+                        placeholder='location'
+                        required
+                    />
+
+
+
+
+                    <label className={"txt3"}>Category</label>
+                    <select className={"cate"}>
+                      <option>Resturant</option>
+                      <option>Chef</option>
+                    </select>
+
+
+                    <label className={"txt3"}>Work Hours</label>
+                    <div className={"work"}>
+                      <div className={"workHours"}>
+                        <p className={'inittxt'}>From:</p>
+                        <input 
+                          className='workinput'
+                          type='text'
+                          name='location'
+                          required
+                        />
+                        <GoClock  className={"clock"}/>
+                      </div>
+
+                      <div className={"workHours"}>
+                        <p className={'inittxt'}>To:</p>
+                        <input 
+                          className='workinput'
+                          type='text'
+                          name='location'
+                          required
+                        />
+                        <GoClock className={"clock"}/>
+                      </div>
+                    </div>
+
+
+                  <label className={"txt3"}>Work Days</label>
+                  <div className={"work"}>
+                    <div className={"workHours"}>
+                      <p className={'inittxt'}>From:</p>
+                      <input 
+                        className='workinput'
+                        type='text'
+                        name='location'
+                        required
+                      />
+                      <FaRegCalendarAlt  className={"clock"}/>
+                    </div>
+
+
+                    <div className={"workHours"}>
+                      <p className={'inittxt'}>To:</p>
+                      <input 
+                        className='workinput'
+                        type='text'
+                        name='location'
+                        required
+                      />
+                        <FaRegCalendarAlt className={"clock"}/>
+                    </div>
+                  </div>
+
+                  </div>
+                </section>
+              </div>
+
+          </form>
+          
+        </div>
       
-      </div>
-    )
-  }
+    </div>
+  )
+}
